f22c9c8b798301eeac576baa544cc4eb6f2dd9b7
peter <petrel.harp@gmail.com>
2019-07-13 23:09:40 -0700

    a good fig

--- figures/plot_sweep.py
+++ figures/plot_sweep.py
@@ -17,10 +17,10 @@ if len(sys.argv) != 3:
 treefile = sys.argv[1]
 Ne = int(sys.argv[2])
 
-seed = 23
 mut_rate = 1e-9
+nreps = 20
 
-outbase = ".".join(treefile.split(".")[:-1]) + ".{}".format(mut_rate)
+outbase = ".".join(treefile.split(".")[:-1])
 newtrees = outbase + ".recap.trees"
 
 try:
@@ -28,44 +28,203 @@ try:
 except:
     ots = pyslim.load(treefile)
     rts = ots.recapitate(1e-8, Ne=Ne)
-    mts = pyslim.SlimTreeSequence(msprime.mutate(rts, rate=mut_rate, random_seed=seed, keep=True))
-    ts = mts.simplify()
+    ts = rts.simplify()
     ts.dump(newtrees)
 
 # get swept mutations and selection coefficients
-swept_id = np.where(np.diff(ts.tables.mutations.metadata_offset) > 0)[0]
-swept = [ts.mutation(m) for m in swept_id]
-s = np.array([mut.metadata[0].selection_coeff for mut in swept])
-spos = np.array([ts.site(mut.site).position for mut in swept])
+s = np.array([mut.metadata[0].selection_coeff for mut in ts.mutations()])
+spos = np.array([ts.site(mut.site).position for mut in ts.mutations()])
 
 windows = np.arange(0, ts.sequence_length, 5e5)
 windows[-1] = ts.sequence_length
 x = windows[:-1] + np.diff(windows)/2
-site_div = ts.diversity([ts.samples()], windows=windows, mode='site')
-branch_div = mut_rate * ts.diversity([ts.samples()], windows=windows, mode='branch')
 
-fig = plt.figure(figsize=(6, 4))
-ax = fig.add_subplot(111)
-maxy = 1.1 * max(np.max(np.abs(site_div)), np.max(np.abs(branch_div)))
+########################
+# Plot lines for 
+
+all_branch_div = ts.diversity([ts.samples()], windows=windows, mode='branch')
+
+# independent versions of the same rate, low
+
+low_mut_rate = mut_rate
+all_site_div = np.zeros((len(windows) - 1, nreps))
+for k in range(nreps):
+    mts = pyslim.SlimTreeSequence(msprime.mutate(ts, rate=low_mut_rate, keep=True))
+    all_site_div[:, k] = (1/low_mut_rate) * mts.diversity([ts.samples()], windows=windows, mode='site')[:, 0]
+
+# and independent versions of the same rate, high
+
+high_mut_rate = mut_rate * 10
+all_site_div_high = np.zeros((len(windows) - 1, nreps))
+for k in range(nreps):
+    mts = pyslim.SlimTreeSequence(msprime.mutate(ts, rate=high_mut_rate, keep=True))
+    all_site_div_high[:, k] = (1/high_mut_rate) * mts.diversity([ts.samples()], windows=windows, mode='site')[:, 0]
+
+# and for independent subsamples
+
+sample_size = 100
+assert(sample_size * nreps <= ts.num_samples)
+sord = np.array(list(ts.samples()))
+np.random.shuffle(sord)
+sample_sets = []
+for k in range(nreps):
+    sample_sets.append(sord[k * sample_size + np.arange(sample_size)])
+
+subts = ts.simplify([x for y in sample_sets for x in y])
+
+mts = pyslim.SlimTreeSequence(msprime.mutate(subts, rate=mut_rate, keep=True))
+sub_branch_div = ts.diversity(sample_sets, windows=windows, mode='branch')
+sub_site_div = (1/mut_rate) * mts.diversity(sample_sets, windows=windows, mode='site')
+
+maxy = 0.7 * max(np.max(np.abs(all_site_div)), np.max(np.abs(all_branch_div)),
+                 np.max(np.abs(sub_site_div)), np.max(np.abs(sub_branch_div)))
+
+
+######## Plot
+fig = plt.figure(figsize=(6, 6))
+
+# indep't mutations: low
+ax = fig.add_subplot(311)
 ax.set_ylim(0.0, maxy)
-sl, = ax.plot(x, site_div,
-             alpha=1.0,
+sl, = ax.plot(x, all_site_div[:,0],
+             alpha=0.5,
              color='black',
              label="site")
-bl, = ax.plot(x, branch_div,
+sll = ax.plot(x, all_site_div[:,1:],
+             alpha=0.5,
+             color='black')
+bl, = ax.plot(x, all_branch_div,
              alpha=1.0,
              color='red',
              label="branch")
-sl = ax.axvline(spos[0], linestyle=":", label="sweep")
+vl = ax.axvline(spos[0], linestyle=":", label="sweep")
 for k, (pos, sval) in enumerate(zip(spos, s)):
     if k > 0:
-        ax.axvline(pos, linestyle=":")
+        vll = ax.axvline(pos, linestyle=":")
+    # # text labels with selection coefficients?
+    # at = ax.text(pos, 0.95*maxy, "s={:.2}".format(sval))
+
+ax.set_xlabel("chromosome position (bp)")
+ax.set_ylabel("mean diversity (π)")
+leg = ax.legend(
+         loc = "upper left",
+         bbox_to_anchor = (1.01, 1.01),
+         frameon = False,
+         borderpad=0)
+atl = ax.text(ts.sequence_length, maxy, "μ={:.0}".format(low_mut_rate),
+              horizontalalignment='right',
+              verticalalignment='top')
+
+# indep't mutations: high
+ax = fig.add_subplot(312)
+ax.set_ylim(0.0, maxy)
+sl, = ax.plot(x, all_site_div_high[:,0],
+             alpha=0.5,
+             color='black',
+             label="site")
+sll = ax.plot(x, all_site_div_high[:,1:],
+             alpha=0.5,
+             color='black')
+bl, = ax.plot(x, all_branch_div,
+             alpha=1.0,
+             color='red',
+             label="branch")
+vl = ax.axvline(spos[0], linestyle=":", label="sweep")
+for k, (pos, sval) in enumerate(zip(spos, s)):
+    if k > 0:
+        vll = ax.axvline(pos, linestyle=":")
+    # # text labels with selection coefficients?
+    # at = ax.text(pos, 0.95*maxy, "s={:.2}".format(sval))
+
+ax.set_xlabel("chromosome position (bp)")
+ax.set_ylabel("mean diversity (π)")
+leg = ax.legend(
+         loc = "upper left",
+         bbox_to_anchor = (1.01, 1.01),
+         frameon = False,
+         borderpad=0)
+atl = ax.text(ts.sequence_length, maxy, "μ={:.0}".format(high_mut_rate),
+              horizontalalignment='right',
+              verticalalignment='top')
+
+
+# independant subsamples
+ax = fig.add_subplot(313)
+ax.set_ylim(0.0, maxy)
+sl, = ax.plot(x, sub_site_div[:,0],
+             alpha=0.5,
+             color='black',
+             label="site")
+sll = ax.plot(x, sub_site_div[:,1:],
+             alpha=0.5,
+             color='black')
+bl, = ax.plot(x, sub_branch_div[:,0],
+             alpha=0.5,
+             color='red',
+             label="branch")
+bll = ax.plot(x, sub_branch_div[:,1:],
+             alpha=0.5,
+             color='red')
+vl = ax.axvline(spos[0], linestyle=":", label="sweep")
+for k, (pos, sval) in enumerate(zip(spos, s)):
+    if k > 0:
+        vll = ax.axvline(pos, linestyle=":")
     # # text labels with selection coefficients?
     # at = ax.text(pos, 0.95*maxy, "s={:.2}".format(sval))
 
 ax.set_xlabel("chromosome position (bp)")
 ax.set_ylabel("mean diversity (pi)")
-ax.legend()
-fig.savefig(outbase + ".diversity.pdf", bbox_inches = "tight")
+leg = ax.legend(
+         loc = "upper left",
+         bbox_to_anchor = (1.01, 1.01),
+         frameon = False,
+         borderpad=0)
+atl = ax.text(ts.sequence_length, maxy, "μ={:.0}\n n={}".format(mut_rate, sample_size),
+              horizontalalignment='right',
+              verticalalignment='top')
+
+# done
+fig.savefig("{}.{}.diversity.pdf".format(outbase, mut_rate), bbox_inches = "tight")
 plt.close(fig)
 
+
+#### other plots
+
+if False:
+    # mutations at different rates
+    mut_rates = [u * mut_rate for u in (0.1, 1, 10, 100)]
+    mut_site_div = np.zeros((len(windows) - 1, len(mut_rates)))
+    mts = ts
+    for k in range(len(mut_rates)):
+        rate = mut_rates[k]
+        drate = rate - ([0.0] + mut_rates)[k]
+        mts = pyslim.SlimTreeSequence(msprime.mutate(ts, rate=drate, keep=True))
+        mut_site_div[:, k] = (1/rate) * mts.diversity([ts.samples()], windows=windows, mode='site')[:, 0]
+
+    # diff mut rates
+    ax = fig.add_subplot(311)
+    ax.set_ylim(0.0, maxy)
+    mut_colors = plt.cm.brg(np.linspace(0, 0.4, len(mut_rates)))
+    sll = [ax.plot(x, mut_site_div[:, k],
+                   alpha=0.8,
+                   color=mut_colors[k])[0] for k in range(len(mut_rates))]
+    bl, = ax.plot(x, all_branch_div,
+                 alpha=1.0,
+                 color='red',
+                 label="branch")
+    vl = ax.axvline(spos[0], linestyle=":", label="sweep")
+    for k, (pos, sval) in enumerate(zip(spos, s)):
+        if k > 0:
+            vll = ax.axvline(pos, linestyle=":")
+        # # text labels with selection coefficients?
+        # at = ax.text(pos, 0.95*maxy, "s={:.2}".format(sval))
+
+    ax.set_xlabel("chromosome position (bp)")
+    ax.set_ylabel("mean diversity (π)")
+    leg = ax.legend(
+             sll + [bl, vl],
+             ["mu = {:.0}".format(u) for u in mut_rates] + ["branch", "sweep"],
+             loc = "upper left",
+             bbox_to_anchor = (1.01, 1.01),
+             frameon = False,
+             borderpad = 0)

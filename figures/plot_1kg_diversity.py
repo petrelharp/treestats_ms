0c7e85f8547c19c62e97de384b4648a014ad0e9b
peter <petrel.harp@gmail.com>
2019-08-02 15:24:13 -0700

    plot diversity as executable

--- figures/plot_1kg_diversity.py
+++ figures/plot_1kg_diversity.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
 import tskit, tszip, json
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import os, sys
 
-window_width = 5e5
+usage = """
+Usage:
+    {} treefile window_width
+""".format(sys.argv[0])
+
+if len(sys.argv) != 3:
+    raise ValueError(usage)
+
+treefile = sys.argv[1]
+window_width = float(sys.argv[2])
+
+outbase = ".".join(treefile.split(".")[:-1]) + f".{int(window_width)}"
 
 popstyles = {"EAS" : (0, ()),
              "EUR" : (0, (1, 2)),
@@ -15,42 +28,41 @@ colors = plt.get_cmap("tab20").colors
 pop_names = ['CHB', 'JPT', 'CHS', 'CDX', 'KHV', 'CEU', 'TSI', 'FIN', 'GBR', 'IBS', 'YRI', 'LWK', 'GWD', 'MSL', 'ESN', 'ASW', 'ACB', 'MXL', 'PUR', 'CLM', 'PEL', 'GIH', 'PJL', 'BEB', 'STU', 'ITU']
 popcolors = { k : colors[j % len(colors)] for j, k in enumerate(pop_names) }
 
-for chrom in range(1, 2):
-    ts = tskit.load("1kg/1kg_chr{}.trees".format(chrom))
+ts = tskit.load(treefile)
 
-    pop_metadata = [json.loads(pop.metadata) for pop in ts.populations()]
-    pn = [x['name'] for x in pop_metadata]
-    for a,b in zip(pn, pop_names):
-        assert(a == b)
+pop_metadata = [json.loads(pop.metadata) for pop in ts.populations()]
+pn = [x['name'] for x in pop_metadata]
+for a,b in zip(pn, pop_names):
+    assert(a == b)
 
-    superpops = [x['super_population'] for x in pop_metadata]
-    pop = [ts.node(ind.nodes[0]).population for ind in ts.individuals()]
+superpops = [x['super_population'] for x in pop_metadata]
+pop = [ts.node(ind.nodes[0]).population for ind in ts.individuals()]
 
-    pop_nodes = [[] for _ in range(ts.num_populations)]
-    for ind in ts.individuals():
-        pop_nodes[pop[ind.id]].extend(ind.nodes)
+pop_nodes = [[] for _ in range(ts.num_populations)]
+for ind in ts.individuals():
+    pop_nodes[pop[ind.id]].extend(ind.nodes)
 
-    num_windows = int(ts.sequence_length / window_width)
-    windows = np.linspace(0, ts.sequence_length, num_windows)
-    x = windows[:-1] + np.diff(windows)/2
+num_windows = int(ts.sequence_length / window_width)
+windows = np.linspace(0, ts.sequence_length, num_windows)
+x = windows[:-1] + np.diff(windows)/2
 
-    bdiv = ts.diversity(pop_nodes, windows=windows, mode="branch")
+bdiv = ts.diversity(pop_nodes, windows=windows, mode="branch")
 
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.add_subplot(111)
-    for j in range(bdiv.shape[1]):
-        dl = ax.plot(
-                x, bdiv[:, j], label=pop_names[j],
-                linestyle=popstyles[superpops[j]],
-                color=popcolors[pop_names[j]])
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot(111)
+for j in range(bdiv.shape[1]):
+    dl = ax.plot(
+            x, bdiv[:, j], label=pop_names[j],
+            linestyle=popstyles[superpops[j]],
+            color=popcolors[pop_names[j]])
 
-    leg = ax.legend(
-             fontsize = "small",
-             loc = "upper left",
-             handlelength=3,
-             bbox_to_anchor = (1.01, 1.01),
-             frameon = False,
-             borderpad=0)
-    fig.savefig("1kg/1kg_chr{}.diversity.pdf".format(chrom), bbox_inches = "tight")
-    plt.close(fig)
+leg = ax.legend(
+         fontsize = "small",
+         loc = "upper left",
+         handlelength=3,
+         bbox_to_anchor = (1.01, 1.01),
+         frameon = False,
+         borderpad=0)
+fig.savefig("{}.diversity.pdf".format(outbase), bbox_inches = "tight")
+plt.close(fig)
 

2dc3b63292c95fa61f4c2293f1227fc74919eae4
Jerome Kelleher <jk@well.ox.ac.uk>
2019-09-02 14:57:01 +0100

    Updated figure to use two states.

--- illustrate_algorithm.py
+++ illustrate_algorithm.py
@@ -11,11 +11,14 @@ import subprocess
 def convert_svg(svg_file):
     pdf_file = svg_file.replace(".svg", ".pdf")
     print("converting", svg_file, pdf_file)
-    subprocess.check_call(f"rsvg-convert {svg_file} -o {pdf_file}", shell=True)
+    # subprocess.check_call(f"rsvg-convert {svg_file} -o {pdf_file}", shell=True)
+    # Inkscape does a better job of outputing vector graphics.
+    subprocess.check_call(f"inkscape {svg_file} --export-pdf={pdf_file}", shell=True)
 
 def print_latex_table(pi, beta, x, F):
     print("\\begin{tabular}{c|cccc}")
     print("& $\pi$ & $\\beta$ & $x$ & $F$\\\\")
+    print("\\hline")
     for u in range(len(pi)):
         print(f"{u} & {pi[u]} & {beta[u]} & {x[u]} & {F[u]}\\\\")
     print("\\end{tabular}")
@@ -35,8 +38,7 @@ def branch_stat_algorithm(ts, f, S, w):
         F[u] = f(x[u])
 
     tree = tskit.Tree(ts)
-    # TODO Better SVG for not drawing this.
-    edge_attrs = {u: {"stroke": "white"} for u in range(ts.num_nodes)}
+    edge_attrs = {}
 
     for (t_left, t_right), edges_out, edges_in in ts.edge_diffs():
         for edge_num, edge in enumerate(edges_out):
@@ -54,7 +56,7 @@ def branch_stat_algorithm(ts, f, S, w):
 
             print("out", edge_num)
             print_latex_table(pi, beta, x, F)
-            edge_attrs[edge.child] = {"stroke": "white"}
+            edge_attrs[edge.child] = {"stroke-opacity": "0"}
             svg_file = f"figures/tree_{tree.index}_out_{edge_num}.svg"
             tree.draw_svg(
                 svg_file,
@@ -77,16 +79,16 @@ def branch_stat_algorithm(ts, f, S, w):
                 s += beta[v] * F[v]
                 v = pi[v]
 
-            print("in", edge_num, edge)
-            print_latex_table(pi, beta, x, F)
-
             edge_attrs[edge.child] = {"stroke": "black"}
-            svg_file = f"figures/tree_{tree.index}_in_{edge_num}.svg"
-            tree.draw_svg(
-                svg_file,
-                max_tree_height="ts",
-                edge_attrs=edge_attrs)
-            convert_svg(svg_file)
+
+        print("TREE IN")
+        print_latex_table(pi, beta, x, F)
+        svg_file = f"figures/tree_{tree.index}_init.svg"
+        tree.draw_svg(
+            svg_file,
+            max_tree_height="ts",
+            edge_attrs=edge_attrs)
+        convert_svg(svg_file)
 
         sigma += (t_right - t_left) * s
 
@@ -105,25 +107,20 @@ if __name__ == "__main__":
     tables = tskit.TableCollection(2)
     for j in range(5):
         tables.nodes.add_row(flags=1, time=0)
-    tables.nodes.add_row(time=1)
-    tables.nodes.add_row(time=1)
-    tables.nodes.add_row(time=2)
-    tables.nodes.add_row(time=3)
+    for j in range(4):
+        tables.nodes.add_row(time=j + 1)
 
-    tables.edges.add_row(0, 2, 6, 2)
-    tables.edges.add_row(0, 2, 6, 1)
-    tables.edges.add_row(0, 1, 6, 3)
-    tables.edges.add_row(0, 1, 7, 0)
-    tables.edges.add_row(0, 2, 7, 6)
-    tables.edges.add_row(0, 2, 8, 4)
+    tables.edges.add_row(0, 2, 5, 2)
+    tables.edges.add_row(0, 2, 5, 1)
+    tables.edges.add_row(0, 2, 6, 3)
+    tables.edges.add_row(0, 2, 6, 4)
+    tables.edges.add_row(0, 2, 7, 5)
+    tables.edges.add_row(0, 1, 7, 6)
     tables.edges.add_row(0, 2, 8, 7)
-
-    tables.edges.add_row(1, 2, 5, 0)
-    tables.edges.add_row(1, 2, 5, 3)
-    tables.edges.add_row(1, 2, 7, 5)
+    tables.edges.add_row(0, 2, 8, 0)
 
     tables.sort()
     ts = tables.tree_sequence()
 
-    print(ts.draw_text())
+    # print(ts.draw_text())
     illustrate(ts)
